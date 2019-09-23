15f1f89dd10a5e5e3888773ec0189c50957c7321
Max <max.z@vrshow.ru>
2019-09-23 21:45:02 +0300

    Первая версия игры

---
+++ Assets/Scripts/MovePiton.cs
@@ -0,0 +1,222 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MovePiton : MonoBehaviour
+{
+    // Перечисляем направления
+    enum Direction : byte { Up, Left, Down, Right };
+
+    // Задаем границы поля игры
+    public static int height = 21;
+    public static int width = 21;
+
+    // Задаем начальное направление движения
+    private Direction pitonMoveDirection = Direction.Down;
+
+    // Начальная скорость питона
+    public float fallSetupTime = 0.3f;
+    private float fallTime;
+
+    // Бок питона и начальное количество блоков
+    public byte pitonPartStartCount;
+    public GameObject pitonPart;
+
+    // Если игра закончилась
+    private bool endGame = false;
+
+    // Пердыдущее время, на котором змея подвинулась
+    private float previousTime;
+
+    // Объект с текстом – конец игры
+    public GameObject endGameMessage;
+
+    // Объект яблочко
+    public GameObject apple;
+
+    private Vector3 firstPitonPartPosition;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Скравем сообщение о конце игры
+        endGameMessage.SetActive(false);
+        // устанавливаем значение следующего движения на начальное
+        fallTime = fallSetupTime;
+
+        // Сохраняем начальное положение головы
+        firstPitonPartPosition = transform.Find("Piton Part (1)").position;
+        // Генерируем части питона (клонируем префабы) на координатах головы до количества, заданного начальной переменной
+        for (int i = 0; i < pitonPartStartCount; i++) Instantiate(pitonPart, firstPitonPartPosition, Quaternion.identity, transform);
+
+        // Проходимся по всем элементам змеи
+        // Чтобы раскрасить голову и текущий хвост
+        // Для старта это опциональное действие (для красоты)
+        int isLast = transform.childCount; // длина змеии
+        bool head = true; // Входим на элемент - голову
+        foreach (Transform children in transform)
+        {
+            if (head) // Если голова
+            {
+                head = false; // дальше все элементы не голова
+                // Меняем цвет головы
+                children.GetComponent<SpriteRenderer>().color = new Color32(177, 196, 130, 255);
+            }
+            // У хвоста меняем цвет для красоты
+            if (--isLast == 0) children.GetComponent<SpriteRenderer>().color = new Color32(77, 196, 30, 255);
+        }
+        // Передвигаем яблоко на новую случайную позицию
+        apple.transform.position = new Vector3(UnityEngine.Random.Range(0, MovePiton.width), UnityEngine.Random.Range(0, MovePiton.height));
+        // Включаем отрисовку
+        apple.GetComponent<SpriteRenderer>().enabled = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Если игра окончена
+        if (endGame)
+            // Если нажат пробле - перезапускаем игру
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                // Снимаем флаг окончания игры
+                endGame = false;
+                // Устанавливаем направление движения вниз
+                pitonMoveDirection = Direction.Down;
+                // Нужно удалить все объекты кроме головы
+                bool head = true;
+                foreach (Transform children in transform)
+                {
+                    if (head)
+                    {
+                        head = false;
+                        // У головы сбрасываем положение
+                        children.position = firstPitonPartPosition;
+                    }
+                    else
+                    {
+                        Destroy(children.gameObject);
+                    }
+                }
+                // Повторно запускаем начальную функци, в ней есть почти все для инициализации игры с нуля
+                Start();
+                // На этом кадре стоит завержить обработку
+                // Иначе будут глюки
+                // выходим из функции Update
+                return;
+            }
+            else return;
+
+        // Если нажата одна из кнопок направления
+        if (Input.GetKeyDown(KeyCode.UpArrow)) pitonMoveDirection = Direction.Up;
+        if (Input.GetKeyDown(KeyCode.LeftArrow)) pitonMoveDirection = Direction.Left;
+        if (Input.GetKeyDown(KeyCode.DownArrow)) pitonMoveDirection = Direction.Down;
+        if (Input.GetKeyDown(KeyCode.RightArrow)) pitonMoveDirection = Direction.Right;
+
+        // Пришло время подвинуть змейку
+        if (Time.time - previousTime > fallTime)
+        {
+
+            bool nyam = false; // голова съела яблоко
+            bool head = true; // голова
+            int isLast = transform.childCount; // хвост
+            Vector3 lastPosition = new Vector3(); // позция предыдущего блока
+            // проходимя по всем блокам змеии, для головы и хвоста есть особые действия
+            foreach (Transform children in transform)
+            {
+                // сохраняем текущее положение блока
+                Vector3 currentPosition = children.position;
+                // Это голова
+                if (head)
+                {
+                    head = false; // все остальные блоки не голова
+                    // Двигаем голову в соответствии с направлением
+                    switch (pitonMoveDirection)
+                    {
+                        case Direction.Up:
+                            children.position += new Vector3(0, 1);
+                            break;
+                        case Direction.Left:
+                            children.position += new Vector3(-1, 0);
+                            break;
+                        case Direction.Down:
+                            children.position += new Vector3(0, -1);
+                            break;
+                        case Direction.Right:
+                            children.position += new Vector3(1, 0);
+                            break;
+                    }
+
+                    // Считываем положение головы и округляем до целых
+                    int roundedX = Mathf.RoundToInt(children.transform.position.x);
+                    int roundedY = Mathf.RoundToInt(children.transform.position.y);
+
+                    // если голова вышла за границы стола
+                    // или наткнулся на кусок хвоста, конец игры
+                    if (roundedX < 0 || roundedX >= width || roundedY < 0 || roundedY >= height || notValidMove(children))
+                    {
+                        // Конец игры
+                        endGame = true;
+                        // Показываем надпись
+                        endGameMessage.SetActive(true);
+                        // Меняем цвет головы
+                        children.GetComponent<SpriteRenderer>().color = new Color32(177, 6, 13, 255);
+                    }
+
+                    // если первый блок наткнулся на яблоко
+                    // Проверяем, видно ли яблоко, и совпадает голова и координаты яблока
+                    if (apple.GetComponent<SpriteRenderer>().enabled && children.transform.position == apple.transform.position)
+                    {
+                        nyam = true; // Значит голова съела яблоко!
+                    }
+                }
+                else // Это не голова!
+                {
+                    // Для не головы все просто: подвинуть блок на место головы или предыдущего блока
+                    children.position = lastPosition;
+                }
+                // нужно сохранить в последнюю позицию положение блока до перемещения
+                // т.к. следующий блок должен будет перемещен ровно в эту позицию
+                lastPosition = currentPosition;
+
+                // Это хвост
+                if (--isLast == 0)
+                {
+                    if (nyam) // Яблоко было съедено!
+                    {
+                        nyam = false; // Возвращаем флаг о съеденом яблоке в исходное состояние
+                        // Генерируем после хвоста еще один блок
+                        Instantiate(pitonPart, children.transform.position, Quaternion.identity, transform);
+                        // Выключаем видимость яблока, мы его съекли!
+                        apple.GetComponent<SpriteRenderer>().enabled = false;
+                        // Сокращаем время хода
+                        // Уменьшаем время на 10%
+                        fallTime *= 0.9f;
+                    }
+                }
+            }
+            previousTime = Time.time; // сохраняем текущее время, на котором произошел ход
+        }
+    }
+
+    // Функция проверки пересечения головы с телом
+    // Получает блок головы, возврящает правду или ложь
+    private bool notValidMove(Transform head)
+    {
+        // Для головы делать проверку с головой не нужно
+        bool isHead = true;
+        foreach (Transform children in transform)
+        {
+            if (isHead) // голову пропускаем
+            {
+                isHead = false; // все остальные блоки не голова
+                continue; // заходим на следующую итерацию цикл
+            }
+            // Если голова и блок по одним координатам - питон съел себя
+            if (head.position == children.position) return true;
+        }
+        return false; // Питон себя не съел
+    }
+}
---
+++ Assets/Scripts/SpawnApple.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnApple : MonoBehaviour
+{
+    public float spawnTime = 3.0f; // Пауза до следующего яблока
+    private float previousTime; // Время съедания предыдущего яблока
+    private bool waitSpawn = false; // Включаем таймер на время ожидания
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Яблоко съедено (объект не рисуется на столе), но таймер не запущен
+        if (!waitSpawn && !gameObject.GetComponent<SpriteRenderer>().enabled)
+        {
+            previousTime = Time.time; // Выставляем момент времени, что яблоко съедено
+            waitSpawn = true; // Запускаем таймер
+        }
+
+        // Таймер запущен, ждем когда он подойдет ко времени
+        // Чтобы показать яблоко
+        if (waitSpawn && Time.time - previousTime > spawnTime)
+        {
+            waitSpawn = false; // выполняем отключение таймера
+            // Получаем новые случайные координаты яблока на поле
+            transform.position = new Vector3(Random.Range(0,MovePiton.width), Random.Range(0, MovePiton.height));
+            // Показываем яблоко
+            gameObject.GetComponent<SpriteRenderer>().enabled = true;
+        }
+    }
+}
